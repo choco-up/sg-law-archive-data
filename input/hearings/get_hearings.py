6fd5e37dbc87dcfc102ac62d623e52b21c82cafa
Adrian Shum <adrian@choco-up.com>
2023-11-21 15:11:35 +0800

    feat: hearings in python scraping

---
+++ input/hearings/get_hearings.py
@@ -0,0 +1,98 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+from populate_hearing_data import populate_hearing_data
+import json
+from datetime import datetime, timedelta
+import math
+import urllib3
+urllib3.disable_warnings()
+
+URL = "https://www.judiciary.gov.sg/hearing-list/GetFilteredList"
+JSON_FILE = "data/hearings.json"
+
+# Set the start and end dates when the script is first run
+selected_start_date = (datetime.now() - timedelta(days=2)).strftime("%Y-%m-%dT%H:%M:%S.000Z")
+selected_end_date = (datetime.now() + timedelta(days=365)).strftime("%Y-%m-%dT%H:%M:%S.000Z")
+
+
+def make_request_body(page=0):
+    return {
+        "model": {
+            "CurrentPage": page,
+            "SelectedCourtTab": "",
+            "SearchKeywords": "",
+            "SearchKeywordsGrouping": "",
+            "SelectedCourt": "",
+            "SelectedLawFirms": [],
+            "SelectedJudges": [],
+            "SelectedHearingTypes": [],
+            "SelectedStartDate": selected_start_date,
+            "SelectedEndDate": selected_end_date,
+            "SelectedPageSize": 500,
+            "SelectedSortBy": ""
+        }
+    }
+
+
+def get_hearing_list_page_raw(page):
+    response = requests.post(URL, json=make_request_body(page), verify=False)
+    content = json.loads(response.text)
+    return BeautifulSoup(content['listPartialView'], 'html.parser')
+
+
+def get_hearing_type(html_el):
+    selection = html_el.select(".hearing-metadata .hearing-type")
+    return selection[0].get_text().strip() if selection else None
+
+
+def format_timestamp(timestamp):
+    return datetime.strptime(timestamp, "%d %b %Y, %I:%M %p").isoformat()
+
+
+def parse_hearing_element(html_el):
+    hearing_metadata_els = html_el.select(".hearing-metadata .metadata-wrapper .metadata")
+    return {
+        "title": html_el.attrs['title'],
+        "link": "https://www.judiciary.gov.sg" + html_el.attrs['href'],
+        "type": get_hearing_type(html_el),
+        "reference": hearing_metadata_els[1].get_text().strip() if len(hearing_metadata_els) > 1 else None,
+        "timestamp": format_timestamp(hearing_metadata_els[0].get_text().strip()),
+        "venue": html_el.select_one(".hearing-item-wrapper .text").get_text().strip(),
+        "coram": html_el.select(".hearing-item-wrapper .text")[1].get_text().strip()
+    }
+
+
+def parse_hearing_list_html(soup):
+    return [parse_hearing_element(el) for el in soup.select("a.list-item")]
+
+
+def get_pagination_status(soup):
+    pagination_status = soup.select_one(".pagination-summary")
+    if pagination_status:
+        total_count = int(re.search(r"Showing results 1-500 of (\d+)", pagination_status.get_text()).group(1))
+        return math.ceil(total_count / 500)
+    return 0
+
+
+def get_hearing_list():
+    first_page_html = get_hearing_list_page_raw(0)
+    additional_pages_count = get_pagination_status(first_page_html)
+    print(f"pages count {additional_pages_count}")
+    hearings = parse_hearing_list_html(first_page_html)
+    for page in range(1, additional_pages_count):
+        page_html = get_hearing_list_page_raw(page)
+        hearings.extend(parse_hearing_list_html(page_html))
+    return hearings
+
+
+def main():
+    hearings = get_hearing_list()
+    # populate_hearing_data would be a separate function or module to process 'hearings' data
+    processed_data = populate_hearing_data(hearings)
+    with open(JSON_FILE, 'w') as f:
+        json.dump(processed_data, f)
+
+
+if __name__ == "__main__":
+    main()
---
+++ input/hearings/populate_hearing_data.py
@@ -0,0 +1,61 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import time
+
+
+def get_hearing_detail_raw(url):
+    response = requests.get(url, verify=False)
+    return response.text
+
+
+def get_field_value(parent_el, field_regex):
+    field_value = parent_el.find_all("div", class_="hearing-item")
+    for field in field_value:
+        label = field.find("div", class_="label")
+        if label and re.search(field_regex, label.get_text(), re.IGNORECASE):
+            text = field.find("div", class_="text")
+            return text.get_text().strip() if text else None
+    return None
+
+
+def parse_party_el(party_el):
+    fields = party_el.find_all("div", class_="hearing-item")
+    role_el, name_el = fields[0].find("div", class_="label"), fields[0].find("div", class_="text")
+    representation_el = fields[1].find("div", class_="text") if len(fields) > 1 else None
+
+    return {
+        "role": role_el.get_text().strip() if role_el else None,
+        "name": name_el.get_text().strip() if name_el else None,
+        "representation": representation_el.get_text().strip() if representation_el else None
+    }
+
+
+def parse_hearing_detail(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    hearing_details_el = soup.find("div", class_="detail-wrapper").find("div", class_="row")
+    parties_els = soup.find_all("div", class_="row hearing-party")
+
+    return {
+        "nature_of_case": get_field_value(hearing_details_el, r"^Nature of case$"),
+        "hearing_type": get_field_value(hearing_details_el, r"^Hearing type$"),
+        "charge_number": get_field_value(hearing_details_el, r"^Charge number$"),
+        "offence_description": get_field_value(hearing_details_el, r"^Offence description$"),
+        "hearing_outcome": get_field_value(hearing_details_el, r"^Hearing outcome$"),
+        "parties": [parse_party_el(el) for el in parties_els]
+    }
+
+
+def populate_hearing_data(hearings):
+    populated_data = []
+    for hearing in hearings:
+        try:
+            link = hearing['link']
+            detail_raw = get_hearing_detail_raw(link)
+            parsed_details = parse_hearing_detail(detail_raw)
+            populated_data.append({**hearing, **parsed_details})
+            print(link)
+        except Exception as e:
+            print(f"Caught exception: {e}")
+            time.sleep(3)
+    return populated_data
